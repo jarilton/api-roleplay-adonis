7bfe65c2c5d1ab188e2ae1c09a10c7109ca17a7f
Jarilton Junior (Jamal) <jariltonpereira02@hotmail.com>
2023-07-01 11:14:49 -0300

    feat: definição dos dados do usuário

--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -3,14 +3,34 @@ import supertest from 'supertest'
 
 const BaseUrl = `http://${process.env.HOST}:${process.env.PORT}`
 
+/* 
+  {
+    "users": {
+      "id:": number,
+      "email": string,
+      "username": string,
+      "password": string,
+      "avatar": string,
+    }
+  }
+*/
+
 test.group('User', () => {
-  test.only('it should create an user', async () => {
+  test.only('it should create an user', async (assert) => {
     const userPayload = {
       email: 'teste@teste.com',
       username: 'teste',
       password: '123456',
+      avatar: 'https://www.images.com/image/1',
     }
 
-    await supertest(BaseUrl).post('/users').send(userPayload).expect(201)
+    const { body } = await supertest(BaseUrl).post('/users').send(userPayload).expect(201)
+
+    assert.exists(body.user, 'Property user not exists')
+    assert.exists(body.user.id, 'Property id not exists')
+    assert.equal(body.user.email, userPayload.email)
+    assert.equal(body.user.username, userPayload.username)
+    assert.equal(body.user.password, userPayload.password)
+    assert.equal(body.user.avatar, userPayload.avatar)
   })
 })
