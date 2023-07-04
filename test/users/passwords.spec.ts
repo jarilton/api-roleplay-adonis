3390cec0cee7f2aaba65fede5e529cc1393efb54
Jarilton Junior (Jamal) <jariltonpereira02@hotmail.com>
2023-07-03 23:07:06 -0300

    feat: Mailer - enviando emails com o Adonis

--- test/users/passwords.spec.ts
+++ test/users/passwords.spec.ts
@@ -1,3 +1,4 @@
+import Mail from '@ioc:Adonis/Addons/Mail'
 import Database from '@ioc:Adonis/Lucid/Database'
 import { UserFactory } from 'Database/factories'
 import test from 'japa'
@@ -9,6 +10,10 @@ test.group('Password', (group) => {
   test.only('it should be able to send forgot password email instructions', async (assert) => {
     const user = await UserFactory.create()
 
+    Mail.trap((message) => {
+      assert.equal(message.subject, 'Recuperação de senha')
+    })
+
     await supertest(BASE_URL)
       .post('/forgot-password')
       .send({
@@ -16,6 +21,8 @@ test.group('Password', (group) => {
         resetPasswordUrl: 'url',
       })
       .expect(204)
+
+    Mail.restore()
   })
 
   group.beforeEach(async () => {
