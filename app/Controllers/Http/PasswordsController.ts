4fc03806d11dfe471fb6fa34e09a5f94cf034a78
Jarilton Junior (Jamal) <jariltonpereira02@hotmail.com>
2023-07-10 23:00:00 -0300

    Criação da rota da operação de redifinição de senha

--- app/Controllers/Http/PasswordsController.ts
+++ app/Controllers/Http/PasswordsController.ts
@@ -34,4 +34,8 @@ export default class PasswordsController {
     })
     return response.noContent()
   }
+
+  public async resetPassword({ request, response }: HttpContextContract) {
+    return response.noContent()
+  }
 }
